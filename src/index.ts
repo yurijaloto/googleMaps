66dde487a526a7d3224a80b51bf6eee72443f7c7
yurijaloto <Yuri.Machado1@ibm.com>
2023-10-29 01:28:33 -0300

    ajuste modules

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { CustomMap } from './Map';
 import { User } from './User';
 import { Company } from './Company';
 
-// const apiKey = 'AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU';
-// const apiKeyNew = 'AIzaSyDmDm3JAITAHvR7TlImn-jAc1MgRZubuu4';
-
 export interface Mappable {
   location: {
     lat: number;
